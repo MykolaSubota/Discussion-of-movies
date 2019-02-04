82e5e4a40b22a070e96fbcac34cb6dc46f43a10b
MykolaSubota <subotamykola121@gmail.com>
2019-02-04 23:26:32 +0200

    change the description of the movie

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,36 @@
+class MoviesController < ApplicationController
+	def index
+		@movies = Movie.all
+	end
+	def show
+		@movie = Movie.find(params[:id])
+	end
+	def new
+		@movie = Movie.new
+	end
+	def create
+		@movie = Movie.new(movie_params)
+		if @movie.save
+			redirect_to @movie
+		end
+	end
+	def edit
+		@movie = Movie.find(params[:id])
+		
+	end
+	def update
+		@movie = Movie.find(params[:id])
+		if @movie.update_attributes(movie_params)
+			redirect_to @movie
+		else
+			render :edit
+			
+		end
+	end
+
+private
+
+def movie_params
+	params.require(:movie).permit(:title, :description)
+end
+end
