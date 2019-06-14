1af1ba23287db8d6207411d18bd3c426851ccdda
MykolaSubota <subotamykola121@gmail.com>
2019-06-14 23:19:09 +0300

    add images

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -11,6 +11,16 @@ ActiveAdmin.register Movie do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-permit_params :title, :description, :genre, :image
+permit_params :title, :description, :genre, :image, movie_gallery: []
+form do |f|
+  f.inputs do
+  	f.input :title
+  	f.input :description
+  	f.input :genre
+  	f.input :image
+    f.input :movie_gallery, as: :file, input_html: { multiple: true }
+  end
+  f.actions
+end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190614092904) do
+ActiveRecord::Schema.define(version: 20190614122054) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20190614092904) do
     t.datetime "updated_at", null: false
     t.string "genre"
     t.string "image"
+    t.string "movie_gallery"
   end
 
   create_table "users", force: :cascade do |t|
