5830e611594fd95c109d3f7a5aa9dbb135fd65dd
GuraStanko <ivanstanko72@gmail.com>
2019-06-16 16:47:03 +0300

    fixing test model and adding test of controllers

--- spec/models/impression_spec.rb
+++ spec/models/impression_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Impression, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user){create(:user)}
+
+  context "validation" do
+
+    it "should not be valid without body" do
+       subject.body = nil
+       expect(subject).to_not be_valid
+    end
+
+    it { should validate_presence_of(:body) }
+  end
+
 end
