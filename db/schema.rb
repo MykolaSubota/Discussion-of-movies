94feb3d6af2aef783ba06744bcce9e673c377a95
MykolaSubota <subotamykola121@gmail.com>
2019-06-17 12:13:21 +0300

    add avatar2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190616144605) do
+ActiveRecord::Schema.define(version: 20190616195706) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 20190616144605) do
     t.string "image"
     t.string "movie_gallery"
     t.integer "year"
-    t.string "country"
     t.string "director"
     t.string "actors"
     t.string "nation"
@@ -71,6 +70,7 @@ ActiveRecord::Schema.define(version: 20190616144605) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "avatar"
+    t.index ["avatar"], name: "index_users_on_avatar", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
